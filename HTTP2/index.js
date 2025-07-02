71a75f74d49f556c56c03f1a8f35271c69ce8b6d
AkshitaKumari156 <ashiakshitaaa@gmail.com>
2025-07-02 19:39:21 +0530

    Changes

---
+++ HTTP2/index.js
@@ -0,0 +1,32 @@
+const express=require("express")
+const app=express()
+
+app.get('/sum/:a/:b',function(req,res){
+    const a=parseInt(req.params.a)
+    const b=parseInt(req.params.b)
+    res.json({
+        answer: a+b
+    })
+})
+app.get('/sub/:a/:b',function(req,res){
+    const a=parseInt(req.params.a)
+    const b=parseInt(req.params.b)
+    res.json({
+        answer: a-b
+    })
+})
+app.get('/mul/:a/:b',function(req,res){
+    const a=(req.params.a)
+    const b=(req.params.b)
+    res.json({
+        answer: a*b
+    })
+})
+app.get('/div/:a/:b',function(req,res){
+    const a=(req.params.a)
+    const b=(req.params.b)
+    res.json({
+        answer: a/b
+    })
+})
+app.listen(3000)
